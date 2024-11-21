d9ee8252abc1e710c5595ce97f1f7b60a72fbeb9
Hogyun Jeon <akzm9999@gmail.com>
2024-11-21 19:08:37 +0900

    feat: yjs context 작성

--- packages/frontend/src/lib/utils.ts
+++ packages/frontend/src/lib/utils.ts
@@ -1,7 +1,25 @@
+import { createContext, useContext } from "react";
+
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export default {};
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function createSafeContext<T>(defaultValue?: T) {
+  const MyContext = createContext<T | undefined>(defaultValue);
+
+  function useMyContext() {
+    const context = useContext(MyContext);
+
+    if (context === undefined) {
+      throw new Error("Provider 없음");
+    }
+
+    return context;
+  }
+
+  return [useMyContext, MyContext.Provider] as const;
 }
