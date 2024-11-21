1d68db81e82138a11141e7dd56f068a783a6b868
fru1tworld <fruitworld.planet@gmail.com>
2024-11-21 21:34:12 +0900

    feat: redis, mongodb setting

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -6,11 +6,35 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { YjsModule } from './yjs/yjs.module';
 import { NoteModule } from './note/note.module';
-
+import { ContentModule } from './content/content.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_HOST'),
+        user: configService.get<string>('MONGO_USER'),
+        pass: configService.get<string>('MONGO_PASSWORD'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+    }),
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        readyLog: true,
+        config: {
+          host: configService.get<string>('REDIS_HOST'),
+          port: configService.get<number>('REDIS_PORT'),
+          password: configService.get<string>('REDIS_PASSWORD'),
+        },
+      }),
+      inject: [ConfigService],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
@@ -31,6 +55,8 @@ import { NoteModule } from './note/note.module';
     SpaceModule,
     YjsModule,
     NoteModule,
+    ContentModule,
+    RedisModule,
   ],
   controllers: [AppController],
   providers: [AppService],
