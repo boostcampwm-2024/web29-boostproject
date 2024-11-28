0a5ea9ceb012d83020ee692faf9f7156803dcaf9
Hogyun Jeon <akzm9999@gmail.com>
2024-11-28 10:45:06 +0900

    refactor: breadcrumb 보여줄 아이템 계산 로직 가독성 개선

--- packages/frontend/src/components/SpaceBreadcrumb.tsx
+++ packages/frontend/src/components/SpaceBreadcrumb.tsx
@@ -21,6 +21,20 @@ type SpacePath = {
   urlPath: string;
 };
 
+function splitSpacePaths(spacePaths: SpacePath[], itemCountToDisplay: number) {
+  // 처음 스페이스는 무조건 보여준다.
+  const firstSpacePath = spacePaths[0];
+
+  // 중간 스페이스들은 ...으로 표시하고, 클릭 시 드롭다운 메뉴로 보여준다.
+  const hiddenSpacePaths = spacePaths.slice(1, -2);
+
+  // 마지막 (n-1)개 스페이스는 무조건 보여준다.
+  const lastItemCount = Math.min(spacePaths.length, itemCountToDisplay - 1);
+  const shownSpacePaths = spacePaths.slice(-lastItemCount);
+
+  return [firstSpacePath, hiddenSpacePaths, shownSpacePaths] as const;
+}
+
 type HiddenItemsProps = {
   spacePaths: SpacePath[];
 };
@@ -85,11 +99,9 @@ export default function SpaceBreadcrumb({
   itemCountToDisplay = 3,
 }: SpaceBreadcrumbProps) {
   // [처음, (...중간...), 직전, 현재]
-
-  const firstSpacePath = spacePaths[0];
-  const hiddenSpacePaths = spacePaths.slice(1, -2);
-  const shownSpacePaths = spacePaths.slice(
-    Math.max(-spacePaths.length + 1, -itemCountToDisplay + 1),
+  const [firstSpacePath, hiddenSpacePaths, shownSpacePaths] = splitSpacePaths(
+    spacePaths,
+    itemCountToDisplay,
   );
 
   return (
