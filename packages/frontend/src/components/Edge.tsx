b0f53ecb9df5d8246fffcc5a341e2cdafb038252
CatyJazzy <ahrefhoo@naver.com>
2024-12-03 13:20:33 +0900

    feat: 간선 삭제 버튼 스타일 수정

--- packages/frontend/src/components/Edge.tsx
+++ packages/frontend/src/components/Edge.tsx
@@ -1,10 +1,62 @@
 import { useEffect, useState } from "react";
-import { Group, Line } from "react-konva";
+import { Circle, Group, Line, Text } from "react-konva";
 
 import Konva from "konva";
+import { KonvaEventObject, Node, NodeConfig } from "konva/lib/Node";
 import type { Edge } from "shared/types";
 
 type EdgeProps = Edge & Konva.LineConfig;
+
+const BUTTON_RADIUS = 12;
+
+type EdgeEditButtonProps = {
+  points: number[];
+  onTap?:
+    | ((evt: KonvaEventObject<PointerEvent, Node<NodeConfig>>) => void)
+    | undefined;
+};
+
+function EdgeEditButton({ points, onTap }: EdgeEditButtonProps) {
+  const [isTouch, setIsTouch] = useState(false);
+
+  useEffect(() => {
+    setIsTouch("ontouchstart" in window || navigator.maxTouchPoints > 0);
+  }, []);
+
+  if (!isTouch || points.length < 4) return null;
+
+  const handleTap = (e: KonvaEventObject<MouseEvent | TouchEvent>) => {
+    if (onTap) {
+      const { target } = e;
+      console.log(target);
+    }
+  };
+
+  const middleX = (points[0] + points[2]) / 2;
+  const middleY = (points[1] + points[3]) / 2;
+
+  return (
+    <Group x={middleX} y={middleY} onTap={handleTap}>
+      <Circle
+        radius={BUTTON_RADIUS}
+        fill="#FAF9F7"
+        stroke="#FFFFFF"
+        strokeWidth={2}
+      />
+      <Text
+        fontSize={16}
+        fill="#787878"
+        text="×"
+        align="center"
+        verticalAlign="middle"
+        width={BUTTON_RADIUS * 2}
+        height={BUTTON_RADIUS * 2}
+        offsetX={BUTTON_RADIUS}
+        offsetY={BUTTON_RADIUS - 1}
+      />
+    </Group>
+  );
+}
 
 function calculateOffsets(
   from: { x: number; y: number },
@@ -66,6 +118,7 @@ export default function Edge({
         name="edge"
         id={id}
       />
+      <EdgeEditButton points={points} />
     </Group>
   );
 }
