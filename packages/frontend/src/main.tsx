90b2d4224bfca4348f9b38c33b9385124a7d8339
Hogyun Jeon <akzm9999@gmail.com>
2024-12-04 15:38:48 +0900

    fix: 더 많은 브라우저에서 WebSocket 연결 문제가 없도록 수정 (#207)
    fix: 상대 url을 websocket에 사용할 수 있도록 폴리필

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import "@/lib/polyfill-relative-urls-websocket.ts";
+
 import App from "./App.tsx";
 import "./index.css";
 
